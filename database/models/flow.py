6da9f4115b7fa268a8bd0ddc637da7052b3d810b
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-08-29 14:48:56 +0545

    minor refactor: all models in the same class

--- database/models/flow.py
+++ database/models/flow.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, Integer, String, DateTime, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
-# Create a declarative base class
 Base = declarative_base()
 class Flow(Base):
     __tablename__ = 'flows'
@@ -10,5 +9,15 @@ class Flow(Base):
     id = Column(Integer, Sequence('flow_id_seq'), primary_key=True)
     name = Column(String(100), nullable=False)
     entry_point = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class FlowRuns(Base):
+    __tablename__ = 'flow_runs'
+    
+    id = Column(Integer, Sequence('flow_id_seq'), primary_key=True)
+    name = Column(String(100), nullable=False)
+    flow_id = Column(String(255), nullable=False)
+    status = Column(String(255), nullable=False, default="QUEUED")
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
