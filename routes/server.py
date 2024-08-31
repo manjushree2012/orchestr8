6d60ab54ee1bbe97f15bcb60d7ace0b2c3282267
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-08-31 11:08:51 +0545

    refactor: move websockets routes to a separate file

---
+++ routes/server.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import index
+import flow_run
+import websocket
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+# Serve static files from the `static` directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(index.router)
+app.include_router(flow_run.router)
+app.include_router(websocket.router)
---
+++ routes/websocket.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from connection_manager import ConnectionManager
+
+router = APIRouter()
+
+manager = ConnectionManager()
+
+@router.websocket("/ws/flow_run")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            # Listen for messages from the logger or other sources
+            data = await websocket.receive_text()
+
+            # Broadcast the message to all connected clients
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        print("Client disconnected")
+
+# @router.websocket("/ws/{flow_run_id}")
+# async def websocket_endpoint(websocket: WebSocket, flow_run_id: str):
+#     await websocket.accept()
+#     try:
+#         last_status = None  # Keep track of the last sent status
+#         while True:
+#             # Simulate status changes
+#             current_status = flow_status_store.get(flow_run_id, 'PENDING')
+#             await websocket.send_text(current_status)
+
+#             await asyncio.sleep(4)
+
+#             await websocket.send_text('RUNNING')
+#             await asyncio.sleep(4)
+#             await websocket.send_text('COMPLETED')
+
+#             break
+        
+#             # Send the status only if it has changed
+#             if current_status != last_status:
+#                 await websocket.send_text(current_status)
+#                 last_status = current_status
+
+#             # If the flow is completed or errored, close the connection
+#             if current_status in ['COMPLETED', 'ERROR']:
+#                 break
+
+#             await asyncio.sleep(1)  # Adjust the polling interval as needed
+#     except WebSocketDisconnect:
+#         print(f"WebSocket disconnected for flow {flow_run_id}")
+#     finally:
+#         await websocket.close()
