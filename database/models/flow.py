6360249f9d4a3b4144b69f08579bc7c4d3a1da3d
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-09-08 14:04:25 +0545

    add new table to store task runs

--- database/models/flow.py
+++ database/models/flow.py
@@ -26,3 +26,14 @@ class FlowRuns(Base):
     status = Column(String(255), nullable=False, default="QUEUED")
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class TaskRuns(Base):
+    __tablename__ = 'task_runs'
+    
+    id = Column(Integer, Sequence('task_runs_id_seq'), primary_key=True)
+    task_run_id = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True)
+    name = Column(String(100), nullable=False)
+    flow_run_id = Column(UUID(as_uuid=True), ForeignKey('flow_runs.flow_run_id'))
+    status = Column(String(255), nullable=False, default="QUEUED")
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
