2444d48f099eebf781cbbd067cb9e8dc33866efc
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-09-16 12:23:38 +0545

    added ORM relationship between flow, fow runs models

--- database/models/flow.py
+++ database/models/flow.py
@@ -4,6 +4,9 @@ from datetime import datetime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import ForeignKey
 import uuid
+from sqlalchemy.orm import relationship
+
+import humanize
 
 Base = declarative_base()
 class Flow(Base):
@@ -16,6 +19,19 @@ class Flow(Base):
     tags = Column(JSON, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    flow_runs = relationship("FlowRuns")
+
+    def to_dict(self):
+        return {
+            'id'         : self.id,
+            'flow_id'    : str(self.flow_id),
+            'name'       : self.name,
+            'entry_point': self.entry_point,
+            'tags'       : self.tags,
+            'created_at' : str(self.created_at),
+            'updated_at' : str(self.updated_at)
+        }
 
 class FlowRuns(Base):
     __tablename__ = 'flow_runs'
@@ -32,6 +48,28 @@ class FlowRuns(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
+    flow = relationship("Flow")
+    task_runs = relationship("TaskRuns")
+
+
+    def to_dict(self):
+        return {
+            'id'         : self.id,
+            'flow_run_id': str(self.flow_run_id),
+            'name'       : self.name,
+            'flow_id'    : str(self.flow_id),
+            'status'     : self.status,
+            'duration'   : self.calculate_duration(),
+            'flow'       : self.flow.to_dict() if self.flow else None,
+            # 'task_runs': [task_run.to_dict() for task_run in self.task_runs]
+        }
+
+    def calculate_duration(self):
+        if self.start_time and self.end_time:
+            delta = self.end_time - self.start_time
+            return humanize.naturaldelta(delta)
+        return None
+
 class TaskRuns(Base):
     __tablename__ = 'task_runs'
     
@@ -42,6 +80,9 @@ class TaskRuns(Base):
     status = Column(String(255), nullable=False, default="QUEUED")
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    flow_run = relationship("FlowRuns")
+
 
 class Log(Base):
     __tablename__ = 'logs'
