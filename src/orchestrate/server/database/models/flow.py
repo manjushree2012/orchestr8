0989907c3e2d53c6ae5b31aae38c43b6ede5f06b
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-09-22 10:42:15 +0545

    bugfix

--- src/orchestrate/server/database/models/flow.py
+++ src/orchestrate/server/database/models/flow.py
@@ -96,6 +96,19 @@ class FlowRuns(Base):
             'durationReadable': self.calculate_duration(),
         }
 
+    def to_dict_dashboard(self):
+        return {
+            'id'              : self.id,
+            'flow_run_id'     : str(self.flow_run_id),
+            'name'            : self.name,
+            'flow'            : self.flow.to_dict() if self.flow else None,
+            'status'          : self.status,
+
+            'started_at'      : humanize.naturaltime(self.start_time) if self.start_time else None,
+            'duration'        : self.calculate_duration_in_seconds(),
+            'durationReadable': self.calculate_duration(),
+        }
+
     def calculate_duration_in_seconds(self):
         if self.start_time and self.end_time:
             delta = self.end_time - self.start_time
