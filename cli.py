0bfce3b237441dbfd926ca0fd558fe75166da0bb
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-09-23 13:07:24 +0545

    added scripts for server start

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 # cli.py
 import click
 import uvicorn
+import os
 
 @click.group()
 def cli():
@@ -11,7 +12,11 @@ def cli():
 def start():
     """Start the FastAPI server."""
     # Importing here to avoid circular dependencies if needed
-    from server.main import app
+    from src.orchestrate.server.main import app
+
+    # Change the working directory to the directory where cli.py is located
+    script_dir = os.path.dirname(__file__)
+    os.chdir(script_dir)
 
     # Start the Uvicorn server programmatically
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
